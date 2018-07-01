77beebd0efea77491451c49ab057c2d09e37629a
Quickz <alexserg@inbox.lv>
2018-07-01 18:13:29 +0300

    updated about page

--- src/sample/AboutPage.java
+++ src/sample/AboutPage.java
@@ -1,10 +1,16 @@
 package sample;
 
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
+
+import java.awt.Desktop;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 public class AboutPage
 {
@@ -18,6 +24,24 @@ public class AboutPage
         stage.getIcons().add(new Image("sample/book_icon.png"));
         stage.setScene(scene);
         stage.showAndWait();
+    }
 
+    @FXML
+    private void openSourceCode()
+    {
+        String url = "https://github.com/Quickz/To-do-list-APP";
+
+        try
+        {
+            if (Desktop.isDesktopSupported())
+            {
+                Desktop.getDesktop().browse(new URI(url));
+            }
+        }
+        catch (Exception e)
+        {
+            System.out.println(e);
+            System.out.println("Error in browsing the source code web page");
+        }
     }
 }
